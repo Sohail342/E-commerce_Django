9d1fe9a29b2d92d796884e04fe0d9422af5275bc
= <sohailahmed34280@gmail.com>
2024-08-15 23:03:13 -0700

    Login and registration functionality Complete

--- account/views.py
+++ account/views.py
@@ -1,42 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import  MyUserCreationForm
+from django.urls import reverse
 
 def signup(request):
-    if request.user.is_authenticated:
-        return redirect('/')
     if request.method == 'POST':
-        form = MyUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            username = form.cleaned_data.get('username') 
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'account/signup.html', {'form': form})
+       form = UserCreationForm(request.POST)
+       if form.is_valid():
+           user = form.save()
+           login(request, user)
+           return redirect(reverse("home"))
     else:
-        form =  MyUserCreationForm()
-        return render(request, 'account/signup.html', {'form': form})
+        form =  UserCreationForm()
+    return render(request, 'account/signup.html', {'form': form})
 
 
 def signin(request):
-    if request.user.is_authenticated:
-        return render(request, 'homepage.html')
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            form = AuthenticationForm(request.POST)
-            return render(request, 'account/signin.html', {'form': form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(user)
+            return redirect(reverse("home"))
     else:
         form = AuthenticationForm()
-        return render(request, 'account/signin.html', {'form': form})
+    return render(request, 'account/signin.html', {'form': form})
 
 
 def signout(request):
