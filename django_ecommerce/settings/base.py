44e692b8bc02bbafdd77542625b191da1e5754ba
SOHAIL AHMAD <sohailahmed34280@gmail.com>
2025-03-28 21:12:18 +0500

    Update base.py

--- django_ecommerce/settings/base.py
+++ django_ecommerce/settings/base.py
@@ -191,7 +191,7 @@ JAZZMIN_SETTINGS = {
     "site_title": "Zevaristan  Admin",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Zevaristan",
+    "site_header": "",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Zevaristan",
