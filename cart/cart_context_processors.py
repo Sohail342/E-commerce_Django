63e7ebb88ddf69e82b5261d3f494c63f0fd1e10a
= <sohailahmed34280@gmail.com>
2025-05-01 17:31:27 +0500

    Config for Productio

--- cart/cart_context_processors.py
+++ cart/cart_context_processors.py
@@ -2,7 +2,8 @@ from .models import Cart
 from .cart_session import SessionCart
 
 def cart_item_count(request):
-    if hasattr(request, 'user') and request.user.is_authenticated:
+    user = getattr(request, 'user', None)
+    if user and user.is_authenticated:
         cart, created = Cart.objects.get_or_create(user=request.user)
         return {'cart_item_count': cart.items.count()}
     else:
