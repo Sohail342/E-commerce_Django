48293c647d76d5bb15bba553b2ca6d4590768dd3
= <sohailahmed34280@gmail.com>
2025-06-06 15:02:07 +0500

    Bug Fix: Update vercel.json

--- django_ecommerce/settings/development.py
+++ django_ecommerce/settings/development.py
@@ -1,5 +1,7 @@
 from .base import *
 from dotenv import load_dotenv
+from urllib.parse import urlparse
+import os
 
 load_dotenv()
 
@@ -8,11 +10,17 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-DATABASES = {
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
-}
+    }
 
 ALLOWED_HOSTS = ['.vercel.app/', "*"]
