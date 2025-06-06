bf7d49c9502e9c5529d18baaed6fad4085279db8
= <sohailahmed34280@gmail.com>
2025-06-06 13:42:59 +0500

    Bug Fix: UI

--- django_ecommerce/wsgi.py
+++ django_ecommerce/wsgi.py
@@ -1,8 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Get the environment setting with a default of 'production' for Vercel
+environment = os.getenv('MODULE_ENVIRONMENT', 'production')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ecommerce.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'django_ecommerce.settings.{environment}')
 
 application = get_wsgi_application()
 app = application
