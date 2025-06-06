50a5f30df5eafb9264a688e8101a4b64d22ca62c
= <sohailahmed34280@gmail.com>
2025-06-06 13:56:34 +0500

    Bug Fix: UI

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Install dependencies
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-# Collect static files
-python manage.py collectstatic --noinput
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
